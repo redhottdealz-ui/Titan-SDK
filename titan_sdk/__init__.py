ca72495496bb108a202af422ddcbdddf57aed23e
redhottdealz-ui <redhottdealz@gmail.com>
2026-06-29 09:45:06 -0400

    Update __init__.py

--- titan_sdk/__init__.py
+++ titan_sdk/__init__.py
@@ -1,7 +1,10 @@
 from .client import TitanClient
+from .operations import OperationRegistry, default_operations
 from .version import SDK_VERSION
 
 __all__ = [
     "TitanClient",
+    "OperationRegistry",
+    "default_operations",
     "SDK_VERSION",
 ]
